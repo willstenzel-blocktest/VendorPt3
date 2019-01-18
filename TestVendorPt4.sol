e6b9c403fa63f5f37da1eff94be635991e8d8edd
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-18 16:14:35 -0500

    Update TestVendorPt4.sol

--- TestVendorPt4.sol
+++ TestVendorPt4.sol
@@ -4,22 +4,24 @@ import "./VendorPt4.sol";
 
 contract TestVendorPt4 is VendorPt4 {
 
+  VendorPt4 vendor = new VendorPt4();
+  
   function test_AddItem() {
-    assert(getItemCount("Hats") == 0);
-    addItem("Hats", 15);
-    assert(getItemCount("Hats") == 15);
-    addItem("Mugs", 5);
-    addItem("Ties", 30);
-    addItem("Scarves", 20);
-    assert(getItemCount("Scarves") == 20);
+    assert(vendor.getItemCount("Hats") == 0);
+    vendor.addItem("Hats", 15);
+    assert(vendor.getItemCount("Hats") == 15);
+    vendor.addItem("Mugs", 5);
+    vendor.addItem("Ties", 30);
+    vendor.addItem("Scarves", 20);
+    assert(vendor.getItemCount("Scarves") == 20);
   }
 
   function test_PurchaseItem() {
-    addItem("Watches", 5);
-    addItem("Sunglasses", 40);
-    addItem("Water Bottles", 18);
-    assert(getItemCount("Sunglasses") == 40);
-    purchaseItem("Sunglasses");
-    assert(getItemCount("Sunglasses") == 39);
+    vendor.addItem("Watches", 5);
+    vendor.addItem("Sunglasses", 40);
+    vendor.addItem("Water Bottles", 18);
+    assert(vendor.getItemCount("Sunglasses") == 40);
+    vendor.purchaseItem("Sunglasses");
+    assert(vendor.getItemCount("Sunglasses") == 39);
   }
 }
