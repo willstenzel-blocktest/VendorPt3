c4c42986a75c430a42f64405018d4ec971bbc9fd
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-18 16:11:39 -0500

    Uploaded contracts

---
+++ VendorPt3.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.4.25;
+
+contract VendorPt3 {
+  struct Item {
+    string name;
+    uint count;
+  }
+
+  address storeOwner;
+  mapping(string => Item) inventory;
+
+  event Purchase(address sender, string itemName);
+
+  constructor() public {
+    storeOwner = msg.sender;
+  }
+
+  modifier inStock(string itemName) {
+    require(inventory[itemName].count > 0);
+    _;
+  }
+
+  function purchaseItem(string itemName) public inStock(itemName) {
+    inventory[itemName].count--;
+    emit Purchase(msg.sender, itemName);
+  }
+
+  function addItem(string itemName, uint itemCount) {
+    require(msg.sender == storeOwner);
+    inventory[itemName] = Item(itemName, itemCount);
+  }
+
+  function getItemCount(string itemName) view public returns (uint count) {
+    return inventory[itemName].count;
+  }
+}
