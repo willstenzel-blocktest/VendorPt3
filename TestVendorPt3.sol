e9490365c44e72b1b3643a4294dc6559bfb356c9
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-22 16:55:12 -0500

    Update TestVendorPt3.sol

--- TestVendorPt3.sol
+++ TestVendorPt3.sol
@@ -7,21 +7,31 @@ contract TestVendorPt3 is VendorPt3 {
   VendorPt3 vendor = new VendorPt3();
   
   function test_AddItem() {
-    assert(vendor.getItemCount("Hats") == 0);
-    vendor.addItem("Hats", 15);
-    assert(vendor.getItemCount("Hats") == 15);
-    vendor.addItem("Mugs", 5);
-    vendor.addItem("Ties", 30);
-    vendor.addItem("Scarves", 20);
-    assert(vendor.getItemCount("Scarves") == 20);
+    assert(getItemCount("Hats") == 0);
+    addItem("Hats", 15);
+    assert(getItemCount("Hats") == 15);
+    addItem("Mugs", 5);
+    addItem("Ties", 30);
+    addItem("Scarves", 20);
+    assert(getItemCount("Scarves") == 20);
   }
 
   function test_PurchaseItem() {
-    vendor.addItem("Watches", 5);
-    vendor.addItem("Sunglasses", 40);
-    vendor.addItem("Water Bottles", 18);
-    assert(vendor.getItemCount("Sunglasses") == 40);
-    vendor.purchaseItem("Sunglasses");
-    assert(vendor.getItemCount("Sunglasses") == 39);
+    addItem("Watches", 5);
+    addItem("Sunglasses", 40);
+    addItem("Water Bottles", 18);
+    addItem("Card", 100);
+    addItem("Computer", 18);
+    addItem("Head Band", 20);
+    addItem("Keyboard", 30);
+    addItem("T shirts", 100);
+    addItem("Sodas", 50);
+    addItem("Marbles", 450);
+    assert(getItemCount("Sunglasses") == 40);
+    purchaseItem("Sunglasses");
+    assert(getItemCount("Sunglasses") == 39);
+    assert(getItemCount("Marbles") == 450);
+    purchaseItem("Marbles");
+    assert(getItemCount("Marbles") == 449);
   }
 }
